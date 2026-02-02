100b448c7bfb0868d6d21adb3e72240ba12c8c48
BASIM THANI <mjnoonha90@gmail.com>
2026-02-02 16:28:10 +0300

    ES6+, Arrow function, Template letrals , Destructing,

--- index.js
+++ index.js
@@ -1,177 +1,273 @@
+// Modern ES6+ Syntax
+// 1.2 Arrow Functions
 
-// Primitives (value types)
-let str = "Hello";          // string
-let num = 42;               // number
-let bool = true;            // boolean
-let nothing = null;         // null
-let notDefined;             // undefined
+function add(a, b) {
+    return a + b
+}
 
-str = "World";          // Reassigning a new string value
+// Arrow function 
+const addNumbers = () => {
+    return a + b
+}
 
-const person = 'rupa'
+const DivieNum = (a , b) => a / b; 
+const SqureNum = a => a * a; 
 
-
-// Reference types
-let obj = { name: "John" }; // object
-let arr = [1, 2, 3];        // array
-let func = () => {};        // function
-
-
-// Type coercion
-// console.log(5 + "5");        // "55" (number → string)
-// console.log("5" == 5);       // true (type coercion)
-// console.log("5" === 5);      // false (strict comparison)
+const lastResult = (l ,f) => {
+    const sum = add(l, f);
+    return sum * 5
+}
 
 
-let a = 10;
-let b = "10";
+// Template letrals 
 
-let c = a + b
+const name = 'Rami'
+const age = 17
 
-let mod = 10 % 3;  
-let divide = 10 / 3; 
+// old school 
+const greet = "This is " + name + ", " + 'he is ' + age + " years old"
 
-let age = 24;
-let hasLicense = false
-let candDrive = age >= 18 && hasLicense;
-let candDrive2 = age >= 18 || hasLicense;
+// new school 
+const greetNewSchool = `This is ${name}, and he's ${age} years old `
 
-let score = 55
-// if (score >= 90) {
-//     console.log("Grade A");
-// } else if (score >= 80) {
-//     console.log("Grade B");
-// } else {
-//     console.log("Grade C");
+// Multi-line strings
+const html = `
+  <div class="user">
+    <h2>${name}</h2>
+    <p>Age: ${age}</p>
+    <p>Next year: ${age + 1}</p>
+  </div>
+`;
+
+
+
+// Destructuring
+
+const colors = ["red", "green", "blue"];
+// const [R, G, B] = colors
+
+const [first, ...others] = colors
+
+const user = {
+    id: 1,
+    name: "Basim Thani",
+    email: "basim.thani@example.com",
+    role: "admin",
+    status: "active",
+    createdAt: "2025-01-12",
+    password: "8435023",
+    comfirm_password: 8435023
+}
+
+const {password, comfirm_password} = user
+
+const registerFunction = () => {
+        if(password == comfirm_password) {
+            console.log('successfull logged in')
+        } else {
+            console.log('invaled cordentials')
+        }
+
+}
+
+// console.log(registerFunction())
+
+
+const student = {
+  id: 1,
+  names: "John",
+  ages: 25,
+  address: {
+    city: "NYC",
+    country: "USA"
+  }
+};
+
+
+const { names, ages, ...otherInfo } = student;
+const { address: { city } } = student; // nested
+
+
+    
+
+
+
+
+
+
+// // Primitives (value types)
+// let str = "Hello";          // string
+// let num = 42;               // number
+// let bool = true;            // boolean
+// let nothing = null;         // null
+// let notDefined;             // undefined
+
+// str = "World";          // Reassigning a new string value
+
+// const person = 'rupa'
+
+
+// // Reference types
+// let obj = { name: "John" }; // object
+// let arr = [1, 2, 3];        // array
+// let func = () => {};        // function
+
+
+// // Type coercion
+// // console.log(5 + "5");        // "55" (number → string)
+// // console.log("5" == 5);       // true (type coercion)
+// // console.log("5" === 5);      // false (strict comparison)
+
+
+// let a = 10;
+// let b = "10";
+
+// let c = a + b
+
+// let mod = 10 % 3;  
+// let divide = 10 / 3; 
+
+// let age = 24;
+// let hasLicense = false
+// let candDrive = age >= 18 && hasLicense;
+// let candDrive2 = age >= 18 || hasLicense;
+
+// let score = 55
+// // if (score >= 90) {
+// //     console.log("Grade A");
+// // } else if (score >= 80) {
+// //     console.log("Grade B");
+// // } else {
+// //     console.log("Grade C");
+// // }
+
+// //  score >= 90 
+// //  ? console.log("Grade A") 
+// //  : score >= 80 
+// //  ? console.log("Grade B") 
+// //  : console.log("Grade C"); 
+
+
+// //  // switch (better for multiple exact matches)
+// // let day = "Monday";
+
+// // switch ((day).toLowerCase) {
+// //     case "Monday":
+// //         console.log("Start of work week");
+// //         break;
+// //     case "Friday":
+// //         console.log("Weekend almost here!");
+// //         break;
+// //     default:
+// //         console.log("Midweek");
+// // }
+// // console.log(typeof(age));
+// // console.log();
+
+
+// // const fruits = ["apple", "banana", "cherry"];
+
+// // for (let i = 0; i < fruits.length; i++) {
+// //     console.log("fruit name:", fruits[i]);
+// // }
+
+
+// // while (age < 30) {
+// //     console.log("Age is less than 30:", age);
+// //     age++;
+// // }
+
+// // let isLoading = true;
+
+// // while (isLoading) {
+// //     console.log("this use is loading...");
+// //     isLoading = false;
+// // }
+
+// // fruits.forEach((fru, i) => {
+// //     console.log("fruit:", fru, "index:", i);
+// // })
+
+// const numbers = [1, 2, 3, 4, -5, 6];
+
+// let totalValue = numbers.reduce((acc, currVal) => {
+//     return acc + currVal
+// }, 0); 
+
+// // console.log("total value:", totalValue);
+
+// let totalNumber2 = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//     totalNumber2 += numbers[i];
 // }
 
-//  score >= 90 
-//  ? console.log("Grade A") 
-//  : score >= 80 
-//  ? console.log("Grade B") 
-//  : console.log("Grade C"); 
-
-
-//  // switch (better for multiple exact matches)
-// let day = "Monday";
-
-// switch ((day).toLowerCase) {
-//     case "Monday":
-//         console.log("Start of work week");
-//         break;
-//     case "Friday":
-//         console.log("Weekend almost here!");
-//         break;
-//     default:
-//         console.log("Midweek");
-// }
-// console.log(typeof(age));
-// console.log();
-
-
-// const fruits = ["apple", "banana", "cherry"];
-
-// for (let i = 0; i < fruits.length; i++) {
-//     console.log("fruit name:", fruits[i]);
+// for(let num of numbers){
+//     totalNumber2 += num;
 // }
 
+// // console.log("total number 2:", totalNumber2);
 
-// while (age < 30) {
-//     console.log("Age is less than 30:", age);
-//     age++;
-// }
+// const users = [
+//   { id: 101, name: "Sara", email: "sara@ex.com", active: true  },
+//   { id: 202, name: "Ali",  email: "ali@ex.com",  active: false },
+//   { id: 303, name: "Lina", email: "lina@ex.com", active: true  },
+// ];
 
-// let isLoading = true;
-
-// while (isLoading) {
-//     console.log("this use is loading...");
-//     isLoading = false;
-// }
-
-// fruits.forEach((fru, i) => {
-//     console.log("fruit:", fru, "index:", i);
+// const filterdData = users.map((user) => {
+//     return ({ id: user.id, name: user.name });
 // })
 
-const numbers = [1, 2, 3, 4, -5, 6];
+// const activUsers = users.filter((user) => !user.active )
+// // const AliData = users.filter((user) => user.name !== "Ali" )
+// const AliData = users.filter(user => user.id == 202 )
 
-let totalValue = numbers.reduce((acc, currVal) => {
-    return acc + currVal
-}, 0); 
+// const linadata = users.find(user => user.name === "Lina" )
 
-// console.log("total value:", totalValue);
+// // console.log('lina data', linadata);
 
-let totalNumber2 = 0;
-for (let i = 0; i < numbers.length; i++) {
-    totalNumber2 += numbers[i];
-}
-
-for(let num of numbers){
-    totalNumber2 += num;
-}
-
-// console.log("total number 2:", totalNumber2);
-
-const users = [
-  { id: 101, name: "Sara", email: "sara@ex.com", active: true  },
-  { id: 202, name: "Ali",  email: "ali@ex.com",  active: false },
-  { id: 303, name: "Lina", email: "lina@ex.com", active: true  },
-];
-
-const filterdData = users.map((user) => {
-    return ({ id: user.id, name: user.name });
-})
-
-const activUsers = users.filter((user) => !user.active )
-// const AliData = users.filter((user) => user.name !== "Ali" )
-const AliData = users.filter(user => user.id == 202 )
-
-const linadata = users.find(user => user.name === "Lina" )
-
-// console.log('lina data', linadata);
-
-const items = [
-    { id: 101, name: "Laptop", price: "31", active: true  },
-    { id: 202, name: "Ali",  price: "431",  active: false },
-    { id: 303, name: "Ali", price: "201", active: true  },
-];
+// const items = [
+//     { id: 101, name: "Laptop", price: "31", active: true  },
+//     { id: 202, name: "Mouse",  price: "431",  active: false },
+//     { id: 303, name: "cable", price: "201", active: true  },
+// ];
 
 
 
-const hasExpensiveItem = items.some((item)=> {
-    return item.price > 200;
-})
+// const hasExpensiveItem = items.some((item)=> {
+//     return item.price > 200;
+// })
 
-const allExpensiveItems = items.every((item)=> {
-    return item.price > 200;
-})
+// const allExpensiveItems = items.every((item)=> {
+//     return item.price > 200;
+// })
 
-const includesLaptop = items.every((item)=> {
-    return item.name === "Laptop";
-})
-
-
-
-arr = ['apple', 'banana', 'cherry'];
-const includesLaptop2 = arr.includes("apple");
+// const isAllLaptop = items.every((item)=> {
+//     return item.name === "Laptop";
+// })
 
 
 
-const ArrayHasAliAndActive = items.filter((item) => item.name === "Ali" && item.active === true);
-const ArrayHasAliOrActive = items.filter((item) => item.name === "Ali" || item.active === true);
+// arr = ['apple', 'banana', 'cherry'];
+// const includesLaptop2 = arr.includes("apple");
 
-// console.log(ArrayHasAliAndActive);
-// console.log(ArrayHasAliOrActive);
 
-const ArrayHasAli = items.filter((item) => item.name === "Ali")
-const activeAli = ArrayHasAli.find((item) => item.active === true);
 
-// const activeAli = items.filter((item) => item.name === "Ali").find((item) => item.active === true);
+// const ArrayHasAliAndActive = items.filter((item) => item.name === "laptop" && item.active === true);
+// const ArrayHaslaptopOrActive = items.filter((item) => item.name === "laptop" || item.active === true);
 
-// const totalPriceoOfAli = items.filter((item) => item.name === "Ali").reduce((acc, currVal) => acc + Number(currVal.price), 0);
-const totalPriceoOfAli = ArrayHasAli.reduce((acc, currVal) => acc + Number(currVal.price), 0);
+// // console.log(ArrayHasAliAndActive);
+// // console.log(ArrayHasAliOrActive);
 
-console.log(totalPriceoOfAli);
-// console.log(activeAli);
+// const ArrayHasAli = items.filter((item) => item.name === "Ali")
+// const activeAli = ArrayHasAli.find((item) => item.active === true);
+
+// // const activeAli = items.filter((item) => item.name === "Ali").find((item) => item.active === true);
+
+// // const totalPriceoOfAli = items.filter((item) => item.name === "Ali").reduce((acc, currVal) => acc + Number(currVal.price), 0);
+// const totalPriceoOfAli = ArrayHasAli.reduce((acc, currVal) => acc + Number(currVal.price), 0);
+
+// // console.log(totalPriceoOfAli);
+// // console.log(activeAli);
 
 
 
@@ -179,16 +275,16 @@ console.log(totalPriceoOfAli);
 
 
 
-// const totalPrice = items.reduce((acc, currVal) => {
-//     return acc + currVal.price;
-// }, 0);
+// // const totalPrice = items.reduce((acc, currVal) => {
+// //     return acc + currVal.price;
+// // }, 0);
 
 
-// console.log('active users', activUsers);
+// // console.log('active users', activUsers);
 
-// console.log('hasExpensiveItem', hasExpensiveItem);
-// console.log('allExpensiveItems', allExpensiveItems);
-// console.log('totalPrice', totalPrice);
-// console.log('includesLaptop', includesLaptop);
-// console.log('includesLaptop2', includesLaptop2);
+// // console.log('hasExpensiveItem', hasExpensiveItem);
+// // console.log('allExpensiveItems', allExpensiveItems);
+// // console.log('totalPrice', totalPrice);
+// // console.log('includesLaptop', includesLaptop);
+// // console.log('includesLaptop2', includesLaptop2);
 
